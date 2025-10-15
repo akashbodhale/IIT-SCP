d6db4d8e74ed69968ee301e91f62e20b38d74cad
akash-bodhale-ITMD <aabodhale@gmail.com>
2025-10-15 12:50:41 -0500

    auth flow

---
+++ src/main/java/illinoistech/itm/web/system/integration/student_collabration_platform/dto/SignUpRequest.java
@@ -0,0 +1,4 @@
+package illinoistech.itm.web.system.integration.student_collabration_platform.dto;
+
+public class SignupRequest {
+}
---
+++ src/main/java/illinoistech/itm/web/system/integration/student_collabration_platform/repository/UserRepository.java
@@ -0,0 +1,4 @@
+package illinoistech.itm.web.system.integration.student_collabration_platform.repository;
+
+public class UserRepository {
+}
