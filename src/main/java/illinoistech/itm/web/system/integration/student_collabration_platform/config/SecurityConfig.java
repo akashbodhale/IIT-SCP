dfcc11d65e09b773ed3cfe17b555d243a5ead417
akash-bodhale-ITMD <aabodhale@gmail.com>
2025-10-15 21:08:05 -0500

    add annotation EnableAutoConfiguration

--- src/main/java/illinoistech/itm/web/system/integration/student_collabration_platform/config/SecurityConfig.java
+++ src/main/java/illinoistech/itm/web/system/integration/student_collabration_platform/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package illinoistech.itm.web.system.integration.student_collabration_platform.config;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -13,6 +16,10 @@ import org.springframework.web.cors.*;
 import java.util.List;
 
 @Configuration
+@EnableAutoConfiguration(exclude = {
+        SecurityAutoConfiguration.class,
+        UserDetailsServiceAutoConfiguration.class
+})
 public class SecurityConfig {
 
     @Bean
